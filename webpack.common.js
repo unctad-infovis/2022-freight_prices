13c9f3b3a2e1c3dcf136fc4b749145287fbf9c29
Teemo Tebest <teemo.tebest@gmail.com>
2022-06-29 17:07:36 +0200

    Added missing files from webpack update

---
+++ webpack.common.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const name = require('./package.json').name;
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const GoogleFontsPlugin = require('@beyonk/google-fonts-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  name:name,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+            }
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules\//,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[hash]-[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(json)$/,
+        use: [
+          {
+            loader: 'json-loader',
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: '' + name + '.min.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true
+  },
+  plugins: [
+    new GoogleFontsPlugin({
+      fonts: [{
+          family: 'Roboto',
+          variants: ['400','700']
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '' + name + '.min.css'
+    }),
+    new HtmlWebPackPlugin({
+      title: name,
+      template: "./src/html/index.html",
+      filename: "./index.html"
+    }),
+  ]
+};
